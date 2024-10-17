e35e6f045e64bc494ff3e81b454ae356fd83f92c
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-10-17 18:01:41 -0300

    feat: implementing resource group module

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+}
+
+module "azure_resource_group" {
+  source = "./modules/azure_resource_group"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "azure_subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
