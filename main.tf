2320293b9b8aa040ef0007d11a84fce9ce5914e2
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-10-21 21:53:59 -0300

    feat: implementing service plan module

--- main.tf
+++ main.tf
@@ -9,3 +9,13 @@ module "azure_resource_group" {
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
 }
+
+module "azure_service_plan" {
+  source              = "./modules/azure_service_plan"
+
+  name                = var.service_plan_name
+  location            = module.azure_resource_group.resource_group_location
+  resource_group_name = module.azure_resource_group.resource_group_name
+  sku_name            = var.service_plan_sku_name
+  os_type             = var.service_plan_os_type
+}
--- modules/azure_service_plan/variables.tf
+++ modules/azure_service_plan/variables.tf
@@ -1,5 +1,10 @@
-variable "azure_subscription_id" {
-  description = "The Azure Subscription ID"
+variable "name" {
+  description = "The name of the App Service Plan"
+  type        = string
+}
+
+variable "location" {
+  description = "The location where the App Service Plan will be created"
   type        = string
 }
 
@@ -8,23 +13,12 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "resource_group_location" {
-  description = "The location of the resource group"
-  type        = string
-  default     = "East US"
-}
-
-variable "service_plan_name" {
-  description = "The name of the App Service Plan"
-  type        = string
-}
-
-variable "service_plan_os_type" {
+variable "os_type" {
   description = "The O/S type for the App Services to be hosted in this plan. Possible values include Windows, Linux, and WindowsContainer. Changing this forces a new resource to be created."
   type        = string
 }
 
-variable "service_plan_sku_name" {
+variable "sku_name" {
   description = "The SKU for the plan. Possible values include B1, B2, B3, D1, F1, I1, I2, I3, I1v2, I2v2, I3v2, I4v2, I5v2, I6v2, P1v2, P2v2, P3v2, P0v3, P1v3, P2v3, P3v3, P1mv3, P2mv3, P3mv3, P4mv3, P5mv3, S1, S2, S3, SHARED, EP1, EP2, EP3, FC1, WS1, WS2, WS3, and Y1."
   type        = string
 }
