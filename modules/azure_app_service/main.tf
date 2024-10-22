3bcf6eb58911f80efd9d07d34c93f89998567a8c
Jonas Lima de Amorim <dev.amorim@outlook.com>
2024-10-22 09:43:15 -0300

    refactor: including app service configurations

--- modules/azure_app_service/main.tf
+++ modules/azure_app_service/main.tf
@@ -3,6 +3,15 @@ resource "azurerm_linux_web_app" "app" {
   location            = var.location
   resource_group_name = var.resource_group_name
   service_plan_id     = var.service_plan_id
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT = "Development"
+  }
 
-  site_config {}
+  site_config {
+    always_on = false
+
+    application_stack {
+      dotnet_version = "8.0"
+    }
+  }
 }
